cda14d20f7085c3005e14914d607344e4d3ef15c
joaomalfatti <joao.cursos1210@gmail.com>
2022-03-06 08:56:29 -0400

    Fizemos um ListHeader, estilizamos e configuramos

--- src/screens/Home/Index.tsx
+++ src/screens/Home/Index.tsx
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 import { CategorySelect } from "../../components/CategorySelect/Index";
 import { Profile } from "../../components/Profile";
 import { ButtonAdd } from "../../components/ButtonAdd";
+import { ListHeader } from "../../components/ListHeader";
 
 import {styles} from './styles';
 
@@ -21,14 +22,18 @@ export function Home(){
                 <Profile />
                 <ButtonAdd />
             </View>
-
             <View>
                 <CategorySelect 
                     categorySelected={category}
                     setCategory={handleCategorySelect}
                 />
+                <View style={styles.content}>
+                    <ListHeader 
+                        title="Partidas agendadas"
+                        subtitle="Total 6"
+                    />
+                </View>
             </View>
-
         </View>
 
     );
