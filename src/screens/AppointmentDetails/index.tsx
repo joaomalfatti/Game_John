6641f9c5f353514bcdacea7f80972bc767363aa1
almeidamarii <marianadealmeidamelo@gmail.com>
2022-03-12 19:04:15 -0400

    Arrumamos o detalhes do servidor, estilizando, configuramos, adicionamos vários elementos para deixar melhor..

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,13 +1,35 @@
 import React from "react";
-import { View } from "react-native";  
+import { ImageBackground, Text, View, FlatList } from "react-native";  
 import {styles} from "./styles";
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
 import { BorderlessButton } from "react-native-gesture-handler";
 import { Fontisto } from '@expo/vector-icons';
 import { theme } from "../../global/styles/theme";
+import BannerImg from '../../assets/banner.png';
+import { ListHeader } from "../../components/ListHeader";
+import { Member } from "../../components/Member";
+import { ListDivider } from "../../components/ListDivider";
+import { ButtonIcon } from "../../components/ButtonIcon";
+
 
 export function AppointmentDetails(){
+
+    const members = [
+        {
+            id: '1',
+            username: 'João Elias',
+            avatar_url: 'https://github.com/joaomalfatti.png',
+            status: 'online'
+        },
+        {
+            id: '2',
+            username: 'Mariana',
+            avatar_url: 'https://github.com/joaomalfatti.png',
+            status: 'offline'
+        }
+    ]
+
     return (
         <Background>
             <Header
@@ -22,6 +44,42 @@ export function AppointmentDetails(){
                     </BorderlessButton>
                 }
             />
+
+            <ImageBackground
+                source ={BannerImg}
+                style={styles.banner}
+            >
+                <View style={styles.bannerContent}>
+                    <Text style={styles.title}>
+                        Lendários
+                    </Text>
+                    <Text style={styles.subtitle}>
+                        É hoje que vamos chegar ao challenger sem perde uma partida da md10
+                    </Text>
+                </View>
+            </ImageBackground>
+
+            <ListHeader 
+                title="Jogadores"
+                subtitle="Total 3"
+            />
+
+            <FlatList 
+                data={members}
+                keyExtractor={item => item.id}
+                renderItem={({ item}) => (
+                    <Member 
+                        data={item}
+                    />
+                )}
+                ItemSeparatorComponent={() => <ListDivider />}
+                style={styles.members}
+            />
+            <View style={styles.footer}>
+                <ButtonIcon     
+                    title="Entrar na partida"
+                />
+            </View>
         </Background>
-    )
+    );
 }
